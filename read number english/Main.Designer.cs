c5ae4ce6ce933e45b70274059f95403b98452772
mishotsiskaridze <mishotsiskaridze@gmail.com>
2023-09-15 16:27:35 +0400

    Add project files.

---
+++ read number english/Main.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace read_number_english
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readNum = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // readNum
+            // 
+            this.readNum.Location = new System.Drawing.Point(607, 312);
+            this.readNum.Name = "readNum";
+            this.readNum.Size = new System.Drawing.Size(75, 23);
+            this.readNum.TabIndex = 0;
+            this.readNum.Text = "Read";
+            this.readNum.UseVisualStyleBackColor = true;
+            this.readNum.Click += new System.EventHandler(this.readNum_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(594, 228);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 674);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.readNum);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readNum;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ read number english/Main.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace read_number_english
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        string[] ones = new string[20] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        string[] tens = new string[9] { "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        string[] array = new string[4] { "hundred", "thousand", "million", "billion" };
+        long hundreds = 0;
+        long thousands = 0;
+        long ten = 0;
+        long remainder10 = 0;
+        long remainder100 = 0;
+        long remainder1000 = 0;
+        long remainder1000000 = 0;
+        long remainder1000000000 = 0;
+        long number = 0;
+        long billions = 0;
+        long millions = 0;
+        string read = "";
+        string readNumber(long number, int length)
+        {
+            if (number == 0)
+            {
+                return "";
+
+            }
+            if (number < 20)
+            {
+                read = ones[number];
+                return read;
+            }
+            if (length == 2)
+            {
+                ten = number / 10;
+                remainder10 = number % 10;
+                if (remainder10 == 0)
+                {
+                    read = tens[ten - 1];
+                    return read;
+                }
+                else
+                {
+                    read = tens[ten - 1] + " " + ones[remainder10];
+                    return read;
+                }
+
+            }
+            if (length == 3)
+            {
+                hundreds = number / 100;
+                remainder100 = number % 100;
+                read = ones[hundreds] + " " + array[0] + " " + readNumber(remainder100, remainder100.ToString().Length);
+                return read;
+            }
+
+            if (length > 3 && length < 7)
+            {
+                thousands = number / 1000;
+                remainder1000 = number % 1000;
+                read = readNumber(thousands, thousands.ToString().Length) + " " + array[1] + " " + readNumber(remainder1000, remainder1000.ToString().Length);
+                return read;
+
+            }
+
+            if (length > 6 && length < 10)
+            {
+                millions = number / 1000000;
+                remainder1000000 = number % 1000000;
+                read = readNumber(millions, millions.ToString().Length) + " " + array[2] + " " + readNumber(remainder1000000, remainder1000000.ToString().Length);
+                return read;
+
+            }
+
+            if (length > 9 && length < 13)
+            {
+                billions = number / 1000000000;
+                remainder1000000000 = number % 1000000000;
+                read = readNumber(billions, billions.ToString().Length) + " " + array[3] + " " + readNumber(remainder1000000000, remainder1000000000.ToString().Length);
+                return read;
+            }
+            return read;
+        }
+
+
+        private void readNum_Click(object sender, EventArgs e)
+        {
+            number = Convert.ToInt64(textBox1.Text);
+            if (number == 0)
+            {
+                MessageBox.Show("zero");
+                return;
+            }
+            MessageBox.Show(readNumber(number, number.ToString().Length));
+        }
+    }
+}
